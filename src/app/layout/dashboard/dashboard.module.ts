013b7eca368881e85adb6822a0bc3bc1299dac2f
sanguineimpasse <10geneius@gmail.com>
2023-11-13 20:30:59 +0800

    added shortcut routing for admin

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AdminComponent } from './admin/admin.component';
 
 
 
