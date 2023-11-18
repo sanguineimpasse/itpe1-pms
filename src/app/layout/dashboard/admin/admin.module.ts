32a8ba1a4b50ceafcfeb135fee8a379c5fde3263
sanguineimpasse <10geneius@gmail.com>
2023-11-18 14:32:19 +0800

    minor adjustments to list-patient and list-doctor

--- src/app/layout/dashboard/admin/admin.module.ts
+++ src/app/layout/dashboard/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CommonModule } from '@angular/common';
 import { DashboardListItemModule } from 'src/app/components/dashboard-list-item/dashboard-list-item.module';
+import { LayoutRoutingModule } from '../../layout-routing.module';
 
 
 
