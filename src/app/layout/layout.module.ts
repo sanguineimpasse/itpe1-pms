ae29fa7c1d1d1d17329312e84157812423403302
sanguineimpasse <10geneius@gmail.com>
2023-11-18 18:10:51 +0800

    fixed error-inducing component declaration

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SharedComponentModule } from './component/shared-component.module';
-import { EditDoctorComponent } from './account-management/edit/edit-doctor/edit-doctor.component';
-import { EditPatientComponent } from './account-management/edit/edit-patient/edit-patient.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent,
-    EditDoctorComponent,
-    EditPatientComponent
+    LayoutComponent
   ],
   imports: [
     CommonModule,
