8b4d0af8af30994f2b7e4aed2e24e6227cabea1c
sanguineimpasse <10geneius@gmail.com>
2023-11-27 15:41:43 +0800

    removed unused imports in layout.module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { SharedComponentModule } from './component/shared-component.module';
-import { AddDoctorComponent } from './account-management/add/add-doctor/add-doctor.component';
-import { AddPatientComponent } from './account-management/add/add-patient/add-patient.component';
 
 @NgModule({
   declarations: [
