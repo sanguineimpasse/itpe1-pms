ae100e2a96ae89b950f22e55330092b003b28f5a
sanguineimpasse <10geneius@gmail.com>
2023-12-01 18:25:07 +0800

    added selector in registration page for account type

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  
   isDoctor: boolean = false;
+
+  selectPatient(){
+    this.isDoctor = false;
+    this.changeForm();
+  }
+  selectDoctor(){
+    this.isDoctor = true;
+    this.changeForm();
+  }
+
+  changeForm(){
+    const patientForm = this.el.nativeElement.querySelector('#patientForm');
+    const doctorForm = this.el.nativeElement.querySelector('#doctorForm');
+    if(this.isDoctor==true){
+      this.renderer.setStyle(patientForm, 'display', 'none');
+      this.renderer.setStyle(doctorForm, 'display', 'flex');
+    }else if(this.isDoctor==false){
+      this.renderer.setStyle(doctorForm, 'display', 'none');
+      this.renderer.setStyle(patientForm, 'display', 'flex');
+    }
+  }
 }
