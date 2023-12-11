fc002aacb5f092e6b3784a4772ca3a90ba17c24b
sanguineimpasse <10geneius@gmail.com>
2023-12-11 20:26:18 +0800

    removed qrlogin component

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
-import { QrloginComponent } from './qrlogin/qrlogin.component';
+
 
 const routes: Routes = [
   {
     path:'',
     component: LoginComponent
   },
-  {
-    path:'qr',
-    component: QrloginComponent
-  }
 ];
 
 @NgModule({
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { QrloginComponent } from './qrlogin/qrlogin.component';
 import { BackButtonModule } from '../components/back-button/back-button.module';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    QrloginComponent
   ],
   imports: [
     LoginRoutingModule,
